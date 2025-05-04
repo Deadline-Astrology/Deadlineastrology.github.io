43218e2944570e6f728e8a7a73c46c3887d50804
deadlineastrology <deadlineastrology@gmail.com>
2025-05-04 17:16:10 -0400

    Update header.js

--- header.js
+++ header.js
@@ -1,12 +1,6 @@
-window.onload = function() {
-  // Fetch the header content from the header.html file
-  fetch('header.html')
-    .then(response => response.text())  // Get the content as text
-    .then(data => {
-      // Insert the header content into the page
-      document.getElementById('header-container').innerHTML = data;
-    })
-    .catch(error => {
-      console.error("Error loading header:", error);
-    });
-};
+// header.js
+fetch('header.html')
+  .then(response => response.text())
+  .then(data => {
+    document.getElementById('header-container').innerHTML = data;
+  });
